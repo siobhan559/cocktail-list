8877394f6adce9a14c23acd127370f881c38957c
Siobhan <siobhanbrown559@gmail.com>
2021-09-29 16:09:04 +0100

    Seeded cocktail db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = "https://raw.githubusercontent.com/maltyeva/iba-cocktails/master/recipes.json"
+
+puts "Cleaning Database"
+Cocktail.delete_all if Rails.env.development?
+puts "Database cleaned"
+
+puts "Inserting new drinks"
+
+read_file = URI.open(url).read
+cocktails = JSON.parse(read_file)
+
+cocktails.each do |cocktail|
+  Cocktail.create!(name: cocktail["name"], glass: cocktail["glass"], preparation: cocktail["preparation"])
+end
+
+puts "#{Cocktail.count} in the db"
