d60897bad3faecea99981d8363792396eee05c89
Siobhan <siobhanbrown559@gmail.com>
2021-09-30 18:29:21 +0100

    model updated to allow photo attachment

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :cocktails, through: :bookmarks
+  has_one_attached :photo
 
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
